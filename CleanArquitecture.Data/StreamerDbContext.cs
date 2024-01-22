cbb07800f59c6db9cb97cd097978936fd9593602
Jose Diomedes <jdlopez237@gmail.com>
2024-01-22 09:33:46 -0500

    first commit

---
+++ CleanArquitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArquitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Channels;
+
+namespace CleanArquitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\; Initial Catalog=Streamer; Integrated Security=True");
+            .LogTo(Console.WriteLine, new[] {Dblo})
+        }
+        public DbSet<Streamer> Streamers { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+
+    }
+}
